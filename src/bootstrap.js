7173111fe20b0e1c33cf30af3dd633f11e1b9b03
Ian Jennings <ian@meetjennings.com>
2018-01-19 17:07:46 -0600

    set pubnub keepalive true

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -573,6 +573,7 @@ module.exports = (ceConfig = {}, pnConfig = {}) => {
         // connects to the global chatroom
         ChatEngine.pnConfig.uuid = uuid;
         ChatEngine.pnConfig.authKey = authKey;
+        ChatEngine.onConfig.keepAlive = true;
 
         ChatEngine.handshake(() => {
             ChatEngine.firstConnect(state);
