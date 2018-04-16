eed5e2699cc1d7df1354903c4c1065246a44e94a
crgbaumgart <crgbaumgart@gmail.com>
2018-04-16 12:54:08 -0700

    Added test to validate me.session.chat state on refresh [CHAT-280]

--- test/integration/main.test.js
+++ test/integration/main.test.js
@@ -400,6 +400,17 @@ describe('history', () => {
 
         let chatHistory = new ChatEngineHistory.Chat('chat-history');
 
+        for (let i = 0; i < 200; i++) {
+
+             chatHistory.emit('tester', {
+                 text: 'hello world ' + i
+             });
+             chatHistory.emit('not-tester', {
+                 text: 'hello world ' + i
+             });
+
+         }
+
         chatHistory.on('$.connected', () => {
 
             let search = chatHistory.search({
@@ -641,6 +652,53 @@ describe('remote chat list', () => {
 
         setTimeout(() => {
             syncChat.leave();
+        }, 5000);
+
+    });
+
+    it('should leave $me.session.chats.custom and not return upon user reconnect', function syncLeaveChat(done) {
+
+        this.timeout(15000);
+
+        let newChannel3 = 'sync-chat3' + new Date().getTime();
+        let syncChat;
+
+        ChatEngineSync.me.session.on('$.chat.leave', (payload) => {
+            if (payload.chat.channel.indexOf(newChannel3) > -1) {
+                // Reconnect to CE with same user
+                ChatEngineSync = require('../../src/index.js').create({
+                    publishKey: pubkey,
+                    subscribeKey: subkey
+                }, {
+                    globalChannel,
+                    enableSync: true,
+                    throwErrors: true
+                });
+
+                ChatEngineSync.connect(username, { works: false }, username);
+
+                let groupRestored = false;
+
+                ChatEngineSync.on('$.group.restored', () => {
+                    assert.isObject(ChatEngineSync.me.session.chats['system']);
+                    assert.equal(ChatEngineSync.me.session.chats['custom'], undefined);
+                    groupRestored = true;
+                });
+
+                // Await the population of $me.session.chats from enableSync
+                setTimeout(() => {
+                    if (groupRestored === true) {
+                        done();
+                    }
+                }, 5000)
+            }
+
+        });
+
+        syncChat = new ChatEngineClone.Chat(newChannel3);
+
+        setTimeout(() => {
+            ChatEngineSync.me.session.chats["custom"][`${globalChannel}#chat#public.#${newChannel3}`].leave();
         }, 5000);
 
     });
